f5e6c0f727bd21823f52f0b451bf0e04556b2902
chase lindsay <chase.lindsay1@gmail.com>
2020-05-30 15:00:01 -0400

    completed oAuth flow

---
+++ client/src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Landing() {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h1>Emaily!</h1>
+			The easiest way to collect feedback from your users
+		</div>
+	);
+}
