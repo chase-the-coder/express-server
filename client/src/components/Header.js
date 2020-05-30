2c3362a5ef47444d9b4b9f934f12829cd94ec271
chase lindsay <chase.lindsay1@gmail.com>
2020-05-30 14:31:13 -0400

    completed oAuth flow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return 'still deciding';
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login With Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a>Logout</a>
+					</li>
+				);
+		}
+	}
+
 	render() {
-		return <div>Header</div>;
+		return (
+			<nav>
+				<div className="nav-wrapper">
+					<a className="left brand-logo">Emaily</a>
+					<ul className="right">{this.renderContent()}</ul>
+				</div>
+			</nav>
+		);
 	}
 }
+
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,10 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+import { FloatingActionButton } from 'materialize-css';
+
+export default function(state = null, action) {
 	switch (action.type) {
+		case FETCH_USER:
+			return action.payload || false;
 		default:
 			return state;
 	}
