e1884294c1cb8c57757501473a4ecb3a209cc1b3
chase lindsay <chase.lindsay1@gmail.com>
2020-06-08 20:04:16 -0400

    finished sendgrid boilerplate

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
-const SurveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 		const survey = new Survey({
 			title,
@@ -19,5 +19,11 @@ module.exports = (app) => {
 		});
 
 		const mailer = new Mailer(survey, surveyTemplate(survey));
+		await mailer.send();
+		await survey.save();
+		req.user.credits -= 1;
+		const user = await req.users.save();
+
+		res.send(user);
 	});
 };
