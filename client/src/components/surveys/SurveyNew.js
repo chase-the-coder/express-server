f933d132ff0b9ecb17376c214b854ab8e3c9e1df
chase lindsay <chase.lindsay1@gmail.com>
2020-06-12 13:15:41 -0400

    created survey form

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
+
+export default class surveyNew extends Component {
+	render() {
+		return (
+			<div>
+				<SurveyForm />
+			</div>
+		);
+	}
+}
