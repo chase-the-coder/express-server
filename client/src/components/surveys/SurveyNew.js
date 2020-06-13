a2aea34a3325bc397a47981f7f8c93bf312328de
chase lindsay <chase.lindsay1@gmail.com>
2020-06-13 15:04:38 -0400

    completed new feedback form

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
-export default class surveyNew extends Component {
+class SurveyNew extends Component {
+	state = { showFormReview: false };
+
+	renderContent() {
+		if (this.state.showFormReview) {
+			return <SurveyFormReview onCancel={() => this.setState({ showFormReview: false })} />;
+		}
+		return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />;
+	}
 	render() {
-		return (
-			<div>
-				<SurveyForm />
-			</div>
-		);
+		return <div>{this.renderContent()}</div>;
 	}
 }
+export default reduxForm({ form: 'surveyForm' })(SurveyNew);
