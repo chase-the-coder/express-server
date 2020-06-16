45cc9e1d250ada56c54b7f5fffdd6097fa5af58a
chase lindsay <chase.lindsay1@gmail.com>
2020-06-16 16:41:15 -0400

    finished dashboard

--- untitled.js
+++ untitled.js
@@ -5,9 +5,27 @@
 // }
 // fetchAlbums();
 
+const surveyRoutes = require('./routes/surveyRoutes');
+
 const fetchAlbums = async () => {
 	const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums');
 	const json = await res.json();
 	console.log(json);
 };
 fetchAlbums();
+
+surveyRoutes.findOne(
+	{
+		id: surveyId,
+		recipients: {
+			$eleMatch: {
+				email: email,
+				responded: false
+			}
+		}
+	},
+	{
+		$inc: { [choice]: 1 },
+		$set: { 'recipients.$.responded': true }
+	}
+);
